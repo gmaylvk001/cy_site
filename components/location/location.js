f742865992a9ed66e2f16117422e6820e458dbb8
muneeswaran <muneeswaran@eywamedia.com>
2025-12-31 15:42:34 +0530

    location_changes_colour

--- components/location/location.js
+++ components/location/location.js
@@ -34,14 +34,14 @@ const [branches, setBranches] = useState([]);
   return (
     <div className="min-h-screen bg-white py-12 px-4 md:px-8">
       <div className="max-w-7xl mx-auto">
-        <h1 className="text-3xl font-bold text-green-800 mb-10">Our Branches</h1>
+        <h1 className="text-3xl font-bold text-[#a3ca43] mb-10">Our Branches</h1>
 
         {/* Branch Grid */}
          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       {branches.map((branch, idx) => (
         <div
   key={branch._id || idx}
-  className="border border-green-300 rounded-lg shadow-sm p-4 hover:shadow-md transition"
+  className="border border-[#a3ca43] rounded-lg shadow-sm p-4 hover:shadow-md transition"
 >
   <h2 className="text-md font-semibold text-gray-800 mb-2">
     {branch.title}
@@ -52,7 +52,7 @@ const [branches, setBranches] = useState([]);
   
   <a
     href={`mailto:${branch.email}?subject=Inquiry&body=Hello, I would like to know more about your services.`}
-    className="text-blue-600 hover:underline"
+    className="text-[#a3ca43] hover:text-[#a3ca43] hover:underline"
   >
     {branch.email}
   </a>
