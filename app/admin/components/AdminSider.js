1752666961d552747931d3876819ed1eabdbdb89
siva <saravanan@eywamedia.com>
2026-02-12 11:48:17 +0530

    admin sider logo change

--- app/admin/components/AdminSider.js
+++ app/admin/components/AdminSider.js
@@ -57,7 +57,7 @@ export default function AdminSider({ collapsed }) {
     { icon: 'mdi:note-text-outline', label: 'Blog', link: 'blog' },
     { icon: 'mdi:account-outline', label: 'User', link: 'user' },
     { icon: 'mdi:phone-outline', label: 'Contact Us', link: 'contact' },
-    { icon: 'material-symbols:reviews-rounded', label: 'Reviews', link: 'reviews' },
+    { icon: 'mdi:star', label: 'Reviews', link: 'reviews' },
 //     {
 //   icon: 'mdi:file-chart-outline', // changed to reports icon
 //   label: 'Reports',
